7999f5e0510ff2dc18771774504cf20e709e2ea8
dim dew <1729307751@qq.com>
2019-10-22 19:32:50 +0800

    RM Create/Close/Open finish

--- RM_Manager.cpp
+++ RM_Manager.cpp
@@ -7,6 +7,7 @@
 #include "stdafx.h"
 #include "RM_Manager.h"
 #include "str.h"
+#include <cassert>
 
 
 RC OpenScan(RM_FileScan *rmFileScan,RM_FileHandle *fileHandle,int conNum,Con *conditions)//初始化扫描
@@ -17,11 +18,13 @@ RC OpenScan(RM_FileScan *rmFileScan,RM_FileHandle *fileHandle,int conNum,Con *co
 
 RC GetNextRec(RM_FileScan *rmFileScan,RM_Record *rec)
 {
+
 	return SUCCESS;
 }
 
 RC GetRec (RM_FileHandle *fileHandle,RID *rid, RM_Record *rec) 
 {
+
 	return SUCCESS;
 }
 
@@ -59,8 +62,9 @@ RC RM_CreateFile (char *fileName, int recordSize)
 	RM_FileHdr* rmFileHdr;
 	// 检查 recordSize 是否合法 
 	// (由于采用链表的方式所以需要在记录之前保存 nextFreeSlot 字段所以加上 sizeof(int))
-	if (recordSize + sizeof(int) > PF_PAGE_SIZE)
-		return RM_RECSIZETOOBIG;
+	if (recordSize + sizeof(int) > PF_PAGE_SIZE || 
+		recordSize < 0)
+		return RM_INVALIDRECSIZE;
 
 	// 1. 计算每一页可以放置的记录个数
 	maxRecordsNum = ( ( PF_PAGE_SIZE - sizeof(RM_PageHdr) ) << 3 ) / ( (recordSize << 3) + 33 );
@@ -80,6 +84,7 @@ RC RM_CreateFile (char *fileName, int recordSize)
 	if ((rc = AllocatePage(&pfFileHandle, &pfPageHandle)))
 		return rc;
 
+	assert(pfPageHandle.pFrame->page.pageNum == 1);
 	rmFileHdr = (RM_FileHdr*)(pfPageHandle.pFrame->page.pData);
 
 	// 5. 初始化一个 RM_FileHdr 结构，写到 pData 指向的内存区
@@ -97,12 +102,70 @@ RC RM_CreateFile (char *fileName, int recordSize)
 
 	return SUCCESS;
 }
+
+// 
+// 目的: 打开文件，建立 RM_fileHandle
+// 1. 调用 PF_Manager::OpenFile，获得一个 PF_FileHandle
+// 2. 通过 PF_FileHandle 来获取1号页面上的 RM_FileHdr 信息
+// 3. 初始化 RM_FileHandle 成员
+//
 RC RM_OpenFile(char *fileName, RM_FileHandle *fileHandle)
 {
+	RC rc;
+	PF_FileHandle pfFileHandle;
+	PF_PageHandle pfPageHandle;
+
+	// 1. 调用 PF_Manager::OpenFile，获得一个 PF_FileHandle
+	// 2. 通过 PF_FileHandle 来获取1号页面上的 RM_FileHdr 信息
+	if ((rc = openFile(fileName, &pfFileHandle)) ||
+		(rc = GetThisPage(&pfFileHandle, 1, &pfPageHandle)))
+		return rc;
+
+	// 3. 初始化 RM_FileHandle 成员
+	fileHandle->bOpen = true;
+	fileHandle->isRMHdrDirty = false;
+	fileHandle->pfFileHandle = pfFileHandle;
+	fileHandle->rmFileHdr = *((RM_FileHdr*)(pfPageHandle.pFrame->page.pData));
+
+	// 4. 释放内存缓冲区
+	if ((rc = UnpinPage(&pfPageHandle)))
+		return rc;
+
 	return SUCCESS;
 }
 
+
+// 
+// 目的: 关闭文件，判断 RM_FileHdr 是否被修改
+// 如果发生了修改， GetThisPage到内存缓冲区，写入该页, 标记为脏之后
+// 调用 PF_Manager::CloseFile() 关闭 RM 文件
+//
 RC RM_CloseFile(RM_FileHandle *fileHandle)
 {
+	RC rc;
+	PF_PageHandle pfPageHandle;
+
+	if (!(fileHandle->bOpen))
+		return RM_FHCLOSED;
+
+	// 判断是否发生了修改
+	if (fileHandle->isRMHdrDirty) {
+		if ((rc = GetThisPage(&(fileHandle->pfFileHandle), 1, &pfPageHandle)))
+			return rc;
+
+		*(pfPageHandle.pFrame->page.pData) = *((char*)&(fileHandle->rmFileHdr));
+		
+		if ((rc = MarkDirty(&pfPageHandle)) ||
+			(rc = UnpinPage(&pfPageHandle)))
+			return rc;
+	}
+
+	// 调用 PF_Manager::CloseFile() 关闭 RM 文件
+	if ((rc = CloseFile(&(fileHandle->pfFileHandle))))
+		return rc;
+
+	fileHandle->bOpen = false;
+	fileHandle->isRMHdrDirty = false;
+
 	return SUCCESS;
 }
